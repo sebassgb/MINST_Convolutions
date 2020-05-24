67f866c9820fcfd2473bbdfdc61e40e72c32ed1e
Sebastian Giraldo <sebassgb@users.noreply.github.com>
2020-05-25 00:26:44 +0200

    Update tensorflow_MNIST.py

--- tensorflow_MNIST.py
+++ tensorflow_MNIST.py
@@ -15,7 +15,7 @@ def train_mnist_conv():
     # YOUR CODE STARTS HERE
     class myCallback(tf.keras.callbacks.Callback):
      def on_epoch_end(self, epoch, logs={}):
-         if(logs.get('loss')<0.008):
+         if(logs.get('loss')<0.005):
             print("\nReached 99.8% accuracy so cancelling training!")
             self.model.stop_training = True
     # YOUR CODE ENDS HERE
@@ -26,13 +26,15 @@ def train_mnist_conv():
     training_images=training_images.reshape(60000, 28, 28, 1)
     training_images=training_images / 255.0
     test_images = test_images.reshape(10000, 28, 28, 1)
+    #another option training_images = training_images.reshape(-1, 28, 28, 1) / 255.0
+    #another option test_images     =     test_images.reshape(-1, 28, 28, 1) / 255.0  
     test_images=test_images/255.0
     callbacks = myCallback()
     # YOUR CODE ENDS HERE
 
     model = tf.keras.models.Sequential([
             tf.keras.layers.Conv2D(128, (3,3), activation='relu', input_shape=(28, 28, 1)),
-            tf.keras.layers.MaxPooling2D(2,2),
+            tf.keras.layers.MaxPooling2D(2,2), 
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(128, activation='relu'),
             tf.keras.layers.Dense(10, activation='softmax')
@@ -41,7 +43,7 @@ def train_mnist_conv():
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     # model fitting
     history = model.fit(
-        training_images, training_labels, epochs=18, callbacks=[callbacks]
+        training_images, training_labels, epochs=20, callbacks=[callbacks]
     )
     # model fitting
     return history.epoch, history.history['acc'][-1]
