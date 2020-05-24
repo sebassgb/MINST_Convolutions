f1ca716720a40b10f13546a92ddb0a4a2bba7625
Sebastian Giraldo <sebassgb@users.noreply.github.com>
2020-05-25 00:08:50 +0200

    Update tensorflow_MNIST.py

--- tensorflow_MNIST.py
+++ tensorflow_MNIST.py
@@ -9,21 +9,26 @@ sess = tf.Session(config=config)
 
 # GRADED FUNCTION: train_mnist_conv
 def train_mnist_conv():
-  
+    # Please write your code only where you are indicated.
+    # please do not remove model fitting inline comments.
+
+    # YOUR CODE STARTS HERE
     class myCallback(tf.keras.callbacks.Callback):
      def on_epoch_end(self, epoch, logs={}):
-         if(logs.get('loss')<0.02):
+         if(logs.get('loss')<0.008):
             print("\nReached 99.8% accuracy so cancelling training!")
             self.model.stop_training = True
- 
+    # YOUR CODE ENDS HERE
+
     mnist = tf.keras.datasets.mnist
     (training_images, training_labels), (test_images, test_labels) = mnist.load_data(path=path)
-
+    # YOUR CODE STARTS HERE
     training_images=training_images.reshape(60000, 28, 28, 1)
     training_images=training_images / 255.0
     test_images = test_images.reshape(10000, 28, 28, 1)
     test_images=test_images/255.0
     callbacks = myCallback()
+    # YOUR CODE ENDS HERE
 
     model = tf.keras.models.Sequential([
             tf.keras.layers.Conv2D(128, (3,3), activation='relu', input_shape=(28, 28, 1)),
@@ -34,13 +39,11 @@ def train_mnist_conv():
     ])
 
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    model.summary()
     # model fitting
     history = model.fit(
-        model.fit(training_images, training_labels, epochs=20)
+        training_images, training_labels, epochs=18, callbacks=[callbacks]
     )
     # model fitting
     return history.epoch, history.history['acc'][-1]
-    test_loss = model.evaluate(test_images, test_labels)
     
     _, _ = train_mnist_conv()
